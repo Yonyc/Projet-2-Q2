89c6b586430a2056d87e008361e3e05c8d9e48a6
NoahFraiture <94681915+NoahFraiture@users.noreply.github.com>
2022-05-02 21:30:28 +0200

    Update show_graph.py

--- flaskr/show_graph.py
+++ flaskr/show_graph.py
@@ -20,42 +20,6 @@ def isSmaller(date, date2):
             return int(parsed_date[-i]) < int(parsed_date2[-i])
     return None
 
-# Les deux fonctions get_graph_lune() et get_graph_velage() sont extrêment similaire c'est pourquoi c'est en cours
-# de projet de les rassemblé sous une seule et même fonction qui prend en input la fonction calculatrice
-# Par manque de temps cela n'est pas encore fait et sera après les premières review
-@bp.route('/graphs_lune', methods=('GET', 'POST'))
-def get_graph_lune():
-    """
-    :return: HTML page with data as parameters
-    """
-    date_debut = request.form['date_debut']
-    date_fin = request.form['date_fin']
-    famille = request.form['famille']
-
-    db = get_db()
-    data = db.execute('SELECT date, id FROM velages')
-    if famille is None:
-        dic = lune(data, date_debut, date_fin)
-    else:
-        validated_data = []
-        for i in data:
-            animal_id = db.execute('SELECT animal_id FROM animaux_velages WHERE velage_id = ?'), (i[1],)
-            famille_id = db.execute('SELECT famille_id FROM animaux WHERE id = ?'), (animal_id,)
-            famille_nom = db.execute('SELECT nom FROM familles WHERE id = ?'), (famille_id,)
-            if famille_nom == famille:
-                validated_data.append(i)
-        dic = lune(validated_data, date_debut, date_fin)
-
-    keys = dic.keys()
-    values = dic.values()
-    error = "Aucune donnée" if dic == {} else None
-    if error is None:
-        label = "Période"
-        # TODO : afficher le graph avec le dic
-        return render_template('graph.html', keys=keys, values=values, name=label)
-    else: flash(error)
-    return redirect(url_for('accueil'))
-
 #  attention package fullmoon utilisé
 def lune(date_and_id, debut, fin):
     """
@@ -75,35 +39,41 @@ def lune(date_and_id, debut, fin):
         else: unmooned.append(id)
     return {'Pleine Lune':mooned, 'Lune incomplète':unmooned}
 
-@bp.route('/graphs_velage', methods=('GET', 'POST'))
-def get_graph_velage():
+# Les deux fonctions get_graph_lune() et get_graph_velage() sont extrêment similaire c'est pourquoi c'est en cours
+# de projet de les rassemblé sous une seule et même fonction qui prend en input la fonction calculatrice
+# Par manque de temps cela n'est pas encore fait et sera après les premières review
+@bp.route('/graphs_lune', methods=('GET', 'POST'))
+def get_graph_lune():
     """
     :return: HTML page with data as parameters
     """
-    date_debut = request.form['date_debut']
-    date_fin = request.form['date_fin']
-    famille = request.form['famille']
-    db = get_db()
-    data = db.execute('SELECT date, id FROM velages')
-    if famille is None:
-        dic = velages(data, date_debut, date_fin)
-    else:
-        validated_data = []
-        for i in data:
-            animal_id = db.execute('SELECT animal_id FROM animaux_velages WHERE velage_id = ?'), (i[1],)
-            famille_id = db.execute('SELECT famille_id FROM animaux WHERE id = ?'), (animal_id,)
-            famille_nom = db.execute('SELECT nom FROM familles WHERE id = ?'), (famille_id,)
-            if famille_nom == famille:
-                validated_data.append(i)
-        dic = velages(validated_data, date_debut, date_fin)
-    keys = dic.keys()
-    values = dic.values()
-    error = "Aucune donnée" if dic == {} else None
-    if error is None:
-        label = "Période"
-        return render_template('graph.html', keys=keys, values=values, name=label)
-    else:
-        flash(error)
+    if request.method == 'POST':
+        date_debut = request.form['debut_lune']
+        date_fin = request.form['fin_lune']
+        famille = request.form['famille_lune']
+
+        db = get_db()
+        data = db.execute('SELECT date, id FROM velages')
+        if famille is None:
+            dic = lune(data, date_debut, date_fin)
+        else:
+            validated_data = []
+            for i in data:
+                animal_id = db.execute('SELECT animal_id FROM animaux_velages WHERE velage_id = ?'), (i[1],)
+                famille_id = db.execute('SELECT famille_id FROM animaux WHERE id = ?'), (animal_id,)
+                famille_nom = db.execute('SELECT nom FROM familles WHERE id = ?'), (famille_id,)
+                if famille_nom == famille:
+                    validated_data.append(i)
+            dic = lune(validated_data, date_debut, date_fin)
+
+        keys = dic.keys()
+        values = dic.values()
+        error = "Aucune donnée" if dic == {} else None
+        if error is None:
+            label = "Période"
+            # TODO : afficher le graph avec le dic
+            return render_template('graph.html', keys=keys, values=values, name=label)
+        else: flash(error)
     return redirect(url_for('accueil'))
 
 def velages(date_and_id, debut, fin):
@@ -120,29 +90,39 @@ def velages(date_and_id, debut, fin):
             counting[date] = counting.get(date, 0) + 1
     return counting
 
-@bp.route('/graphs_races', methods=('GET', 'POST'))
-def get_graph_races():
+@bp.route('/graphs_velage', methods=('GET', 'POST'))
+def get_graph_velage():
     """
     :return: HTML page with data as parameters
     """
-    races = request.form['races']
-    pourcentage = request.form['pourcentage']
-
-    db = get_db()
-    data = db.execute('SELECT pourcentage, type_id FROM animaux_types')
-    dic = show_races(data, races, pourcentage)
-    keys = dic.keys()
-    values = dic.values()
-    error = "Aucune donnée" if dic == {} else None
-    if error is None:
-        label = "Races"
-        # TODO : afficher le graph avec le dic
-        return render_template('graph.html', keys=keys, values=values, name=label)
-    else:
-        flash(error)
+    if request.method == 'POST':
+        date_debut = request.form['debut_velage']
+        date_fin = request.form['fin_velage']
+        famille = request.form['famille_velage']
+        db = get_db()
+        data = db.execute('SELECT date, id FROM velages')
+        if famille is None:
+            dic = velages(data, date_debut, date_fin)
+        else:
+            validated_data = []
+            for i in data:
+                animal_id = db.execute('SELECT animal_id FROM animaux_velages WHERE velage_id = ?'), (i[1],)
+                famille_id = db.execute('SELECT famille_id FROM animaux WHERE id = ?'), (animal_id,)
+                famille_nom = db.execute('SELECT nom FROM familles WHERE id = ?'), (famille_id,)
+                if famille_nom == famille:
+                    validated_data.append(i)
+            dic = velages(validated_data, date_debut, date_fin)
+        keys = dic.keys()
+        values = dic.values()
+        error = "Aucune donnée" if dic == {} else None
+        if error is None:
+            label = "Période"
+            return render_template('graph.html', keys=keys, values=values, name=label)
+        else:
+            flash(error)
     return redirect(url_for('accueil'))
 
-def show_races(pourcentage_and_races, races:tuple, pourcentage):
+def show_races(pourcentage_and_races, races:list, pourcentage):
     """
     :param pourcentage_and_races: data from get_graph_races()
     :param races: wanted races
@@ -151,6 +131,45 @@ def show_races(pourcentage_and_races, races:tuple, pourcentage):
     """
     counting_race = {}
     for row in pourcentage_and_races:
-        if float(row[0]) >= pourcentage and row[1] in races:
-            counting_race[row[1]] = counting_race.get(row[1],0) + 1
+        for i in range(len(races)):
+            if float(row[0]) >= pourcentage[i] and row[1] == races[i]:
+                counting_race[row[1]] = counting_race.get(row[1],0) + 1
     return counting_race
+
+@bp.route('/graphs_races', methods=('GET', 'POST'))
+def get_graph_races():
+    """
+    :return: HTML page with data as parameters
+    """
+    if request.method == 'POST':
+        holstein = request.form['holstein']
+        jersey = request.form['jersey']
+        bbn = request.form['bbn']
+        holstein_pourcentage = request.form['holstein_pourcentage']
+        jersey_pourcentage = request.form['jersey_pourcentage']
+        bbn_pourcentage = request.form['bbn_pourcentage']
+        races = []
+        pourcentage = []
+        if holstein:
+            races.append("holstein")
+            pourcentage.append(holstein_pourcentage)
+        if jersey:
+            races.append("jersey")
+            pourcentage.append(jersey_pourcentage)
+        if bbn:
+            races.append("bleu-blanc-belge")
+            pourcentage.append(bbn_pourcentage)
+
+        db = get_db()
+        data = db.execute('SELECT pourcentage, type_id FROM animaux_types')
+        dic = show_races(data, races, pourcentage)
+        keys = dic.keys()
+        values = dic.values()
+        error = "Aucune donnée" if dic == {} else None
+        if error is None:
+            label = "Races"
+            # TODO : afficher le graph avec le dic
+            return render_template('graph.html', keys=keys, values=values, name=label)
+        else:
+            flash(error)
+    return redirect(url_for('accueil'))
