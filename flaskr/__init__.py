b9a07a49f79b9ca64b7dc6d44203ba906983db6e
Martimini <94361280+Martimini@users.noreply.github.com>
2022-05-10 15:39:31 +0100

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(pathlib.PAth(__file__).parent.absolute(), "Base_de_donnée/Base_de_donné_sql.db")
+        DATABASE=os.path.join(pathlib.Path(__file__).parent.absolute(), "Base_de_donnée/Base_de_donné_sql.db")
     )
 
     if test_config is None:
